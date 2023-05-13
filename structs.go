68841d0039cc35d84296cceb73b02a958f6389cc
Vikalp Shandilya <greatpk2012@gmail.com>
2023-05-13 08:00:38 +0530

    thy modified

--- structs.go
+++ structs.go
@@ -5,6 +5,12 @@ import (
 )
 
 func main() {
-	
-
+	// make a struct with 3 fields and print it
+	type person struct {
+		first string
+		last string
+		age int
+	}
+	p1 := person{"James", "Bond", 20}
+	fmt.Println(p1)
 }
