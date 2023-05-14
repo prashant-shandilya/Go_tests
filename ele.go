87a62a2b53dd68ac49e4de3eee0317dfae0cb781
Vikalp Shandilya <greatpk2012@gmail.com>
2023-05-14 09:11:59 +0530

    first file

---
+++ ele.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func ele()  bool {
+
+defer fmt.Println("deferred statement executed")
+return true;
+}
+
+func main() {
+	fmt.Printf("I am in main function\n")
+ele()
+
+}
+
